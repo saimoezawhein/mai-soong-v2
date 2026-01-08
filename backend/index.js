418892b4e5ffe7a789b9fc3bc306f9e1b3958ead
Sai Moe Zaw Hein <saimoezawhein9@gmail.com>
2026-01-08 22:38:25 +0700

    port

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'mai_soong',
-  port: process.env.DB_PORT || 3308,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
